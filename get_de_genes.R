e6cc774f61abad2d3e06f6ad79aa798c21988837
AdinaScheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2023-01-31 11:54:21 -0500

    add function to get results of deseq2

---
+++ get_de_genes.R
@@ -0,0 +1,23 @@
+
+# function to extract significant differentially expressed genes
+# dds is a deseq object
+# col is the column of interest
+# cont1 is the first contrast element
+# cont2 is the second contrast element
+get_de_genes <- function(dds, col, cont1, cont2) {
+    
+    # extract de genes
+    dds <- DESeq(dds)
+    
+    # get results from deseq
+    res <- results(dds, contrast = c(col, cont1, cont2))
+    
+    # sort the results by padj
+    res <- res[order(res$padj), ]
+    
+    # save the results to a file
+    write.csv(as.data.frame(res), file = "res1.csv")
+    
+    return(list("dds" = dds, "res" = res))
+}
+
